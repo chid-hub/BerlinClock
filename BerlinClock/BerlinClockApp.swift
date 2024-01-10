8c4624dae7f8b087fa99f7db2a7be3c345ef188f
“chid-hub” <chidhambaramcse@gmail.com>
2024-01-10 13:50:16 +0530

    Creates initial project

---
+++ BerlinClock/BerlinClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockApp.swift
+//  BerlinClock
+//
+//  Created by LoaclUser on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BerlinClockTests/BerlinClockTests.swift
@@ -0,0 +1,14 @@
+//
+//  BerlinClockTests.swift
+//  BerlinClockTests
+//
+//  Created by LoaclUser on 10/01/24.
+//
+
+import XCTest
+@testable import BerlinClock
+
+final class BerlinClockTests: XCTestCase {
+
+    
+}
